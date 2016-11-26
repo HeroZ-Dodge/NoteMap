05529998346f8f63730bf498316a7c5c252180c3
linzheng <linzheng@aipai.com>
2016-11-26 18:35:28 +0800

    xxxxxxx

--- app/src/main/java/com/dodge/hero/notemap/view/activity/impl/MapPointListActivity.java
+++ app/src/main/java/com/dodge/hero/notemap/view/activity/impl/MapPointListActivity.java
@@ -1,121 +1,37 @@
 package com.dodge.hero.notemap.view.activity.impl;
 
-import android.support.annotation.NonNull;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.widget.FrameLayout;
-import android.widget.TextView;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
+import android.view.View;
 
-import com.blankj.utilcode.utils.ToastUtils;
-import com.dodge.hero.commontlibrary.view.activity.BaseMVPActivity;
+import com.dodge.hero.commontlibrary.view.activity.BaseFinishActivity;
 import com.dodge.hero.notemap.R;
-import com.dodge.hero.notemap.data.model.MapPoint;
-import com.dodge.hero.notemap.di.DI;
-import com.dodge.hero.notemap.presenter.MapPointListPresenter;
-import com.dodge.hero.notemap.view.activity.IMapPointListView;
-import com.dodge.hero.notemap.view.adapter.MapPointAdapter;
-import com.dodge.hero.notemap.view.dialog.AddMapPointDialog;
-import com.zhy.adapter.recyclerview.CommonAdapter;
-
-import java.util.List;
-
-import javax.inject.Inject;
+import com.dodge.hero.notemap.view.fragment.impl.MapPointListFragment;
 
 /**
  * 描述:
  * Created by LinZheng on 2016/11/13.
  */
 
-public class MapPointListActivity extends BaseMVPActivity<MapPointListPresenter> implements IMapPointListView {
+public class MapPointListActivity extends BaseFinishActivity {
 
-    public static final String DIALOG_TAG_ADD_MAP_POINT = "map_point";
+    private View mBtnNearBy;
 
-    @Inject
-    MapPointListPresenter mPresenter;
-
-    private TextView mTvNearBy;
-    private TextView mTvHistory;
-    private TextView mTvSetting;
-    private FrameLayout mFlContent;
-    private RecyclerView mRecyclerView;
-
-    private CommonAdapter<MapPoint> mAdapter;
 
     @Override
-    protected MapPointListPresenter getPresenter() {
-        return mPresenter;
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_point_list);
+        mBtnNearBy = findViewById(R.id.tv_near_by);
+        mBtnNearBy.setOnClickListener(v -> {
+            Intent intent = new Intent(MapPointListActivity.this, MapPointListActivity.class);
+            startActivity(intent);
+        });
+        MapPointListFragment fragment = new MapPointListFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.frame_layout, fragment).commit();
     }
-
-    @Override
-    protected int getLayoutRes() {
-        return R.layout.activity_map_point_list;
-    }
-
-    @Override
-    public void initView() {
-        DI.makeActivityComponent(this).inject(this);
-        findViews();
-        setClickListener();
-    }
-
-    private void findViews() {
-        mTvNearBy = (TextView) findViewById(R.id.tv_near_by);
-        mTvHistory = (TextView) findViewById(R.id.tv_history);
-        mTvSetting = (TextView) findViewById(R.id.tv_setting);
-        mFlContent = (FrameLayout) findViewById(R.id.frame_layout_content);
-        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(layoutManager);
-    }
-
-
-    private void setClickListener() {
-        mTvNearBy.setOnClickListener(v -> onNearByClick());
-        mTvHistory.setOnClickListener(v ->
-                ToastUtils.showShortToast(getApplicationContext(), "TODO History"));
-        mTvSetting.setOnClickListener(v ->
-                ToastUtils.showShortToast(getApplicationContext(), "TODO Setting"));
-    }
-
-    private void onNearByClick() {
-        AddMapPointDialog dialog = new AddMapPointDialog();
-        dialog.show(getSupportFragmentManager(), DIALOG_TAG_ADD_MAP_POINT);
-    }
-
-    @Override
-    protected void initData() {
-        mPresenter.attachView(this);
-        mPresenter.present();
-    }
-
-    @NonNull
-    @Override
-    public FrameLayout getContentLayout() {
-        return mFlContent;
-    }
-
-    @Override
-    public void loadFirstPageSuccess(List<MapPoint> mapPoints) {
-        if (mAdapter == null) {
-            mAdapter = new MapPointAdapter(this, R.layout.item_map_point_lsit, mapPoints);
-        }
-        mRecyclerView.setAdapter(mAdapter);
-    }
-
-    @Override
-    public void loadNextPageSuccess(List<MapPoint> mapPoints) {
-
-    }
-
-    @Override
-    public void deleteSuccess() {
-
-    }
-
-    @Override
-    public void deleteFailure() {
-
-    }
-
 
 }
---
+++ commonlibrary/src/main/java/com/dodge/hero/commontlibrary/view/activity/BaseFinishActivity.java
@@ -0,0 +1,28 @@
+package com.dodge.hero.commontlibrary.view.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.dodge.hero.commontlibrary.view.ui.SliderPanel;
+
+/**
+ * 描述
+ * Created by Linzheng on 2016/11/26.
+ */
+
+public abstract class BaseFinishActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ViewGroup decorView = (ViewGroup) getWindow().getDecorView();
+        View contentView = decorView.getChildAt(0);
+        decorView.removeViewAt(0);
+        SliderPanel sliderPanel = new SliderPanel(this, contentView);
+        decorView.addView(sliderPanel, 0);
+    }
+
+}
